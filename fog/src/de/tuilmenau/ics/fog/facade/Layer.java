307de84f066cc31817ac6f0225846c5506ea4707
Florian Liers <florian.liers@tu-ilmenau.de>
2013-04-29 17:15:51 +0200

    Enhanced docu

--- fog/src/de/tuilmenau/ics/fog/facade/Layer.java
+++ fog/src/de/tuilmenau/ics/fog/facade/Layer.java
@@ -21,62 +21,68 @@ import de.tuilmenau.ics.fog.topology.NeighborList;
 
 
 /**
- * Class represents a layer instance and provides access to it.
- * All internal issues of the layer, like addresses, protocols or routes, are hidden.
+ * A layer offers the possibility to announce own services to other peers of
+ * a layer and to access services from others. Moreover, the class provides
+ * methods for retrieving neighbor and capability information that may guide
+ * the usage of a layer.
+ * 
+ * All internal issues of the layer, like addresses, protocols, and routes,
+ * are hidden. Users of a layer are not allowed to get knowledge about such
+ * issues in order to preserve the encapsulation of a layer.
  */
 public interface Layer extends EventSource
 {
 	/**
-	 * Registers a higher layer entity with a given name at the layer.
-	 * Afterwards, clients can connect to this service by using the same name.
+	 * Registers an entity with a given name at the layer. Afterwards,
+	 * clients can connect to this service by using the same name.
 	 * This method does not block.
 	 * 
-	 * @param parentSocket Optional parent socket (might be null if no)
+	 * @param parentSocket Optional parent connection (optional; might be {@code null} if no)
 	 * @param name Name for the service
-	 * @param requirements Description of the service requirements
-	 * @param identity Optional identity of the person requesting the registration of the service
-	 * @return Reference to the service registration (!= null)
-	 * @throws NetworkException on error
+	 * @param requirements Description of the service requirements. These requirements are enforced for all connections to this binding.
+	 * @param identity Optional identity of the requester of the registration
+	 * @return Reference to the service registration ({@code != null})
+	 * @throws NetworkException On error
 	 */
 	public Binding bind(Connection parentSocket, Name name, Description requirements, Identity identity) throws NetworkException;
 
 	/**
-	 * Connects to a higher layer entity with the given name.
+	 * Connects to a {@link Binding} with the given name.
 	 * The method does not block. All feedback is given via events.
 	 * 
-	 * @param name Name of the service, which should be connected to 
+	 * @param name Name of the {@link Binding}, to which should be connected to 
 	 * @param requirements Description of the requirements of the caller for the connection
-	 * @param requester Optional identity of the caller
-	 * @return Reference for the connection (!= null)
-	 * @throws NetworkException on error
+	 * @param requester Optional identity of the caller. It is used for signing the connect request.
+	 * @return Reference for the connection ({@code != null})
+	 * @throws NetworkException On error
 	 */
 	public Connection connect(Name name, Description requirements, Identity requester) throws NetworkException;
 
 	/**
-	 * Checks whether or not a higher layer binding with this name is known by the layer.
-	 * That does not imply that a connection to this name can be constructed.
+	 * Checks whether or not a {@link Binding} with this name is known by the layer.
+	 * That does not imply that {@link #connect} can construct a connection to this name.
 	 * 
 	 * @param name Name to search for
-	 * @return true, if name is known; false otherwise
+	 * @return {@code true}, if name is known; {@code false} otherwise
 	 */
 	public boolean isKnown(Name name);
 	
 	/**
-	 * Determines the capabilities of this layer. Since the whole set of capabilities might be
+	 * Determines the capabilities of this layer. Since the whole set of capabilities may be
 	 * too large, the request can be filtered. Possible filters are the destination name and some
 	 * test requirements. If such filters are present, the method just determines the capabilities
 	 * regarding this destination and these test requirements. 
 	 * 
 	 * @param name Optional destination name to focus the capability analysis
-	 * @param requirements Optional test requirements (if e.g. maximum bandwidth is included in the test requirements, the method will determine the possible bandwidth) 
-	 * @return Capabilities of the layer (!= null)
-	 * @throws NetworkException on error (e.g. filter invalid)
+	 * @param requirements Optional test requirements (if, e.g., maximum bandwidth is included in the test requirements, the method will determine the possible bandwidth) 
+	 * @return Capabilities of the layer ({@code != null})
+	 * @throws NetworkException On error (e.g. filter invalid)
 	 */
 	public Description getCapabilities(Name name, Description requirements) throws NetworkException;
 	
 	/**
-	 * Determines neighbor information about the higher layer entities
-	 * reachable via this layer.
+	 * Determines neighbor information about the {@link Binding}s reachable
+	 * via this layer.
 	 * 
 	 * @param namePrefix Optional filter for the request. If present, only neighbors with a name having this prefix will be listed.
 	 * @return List of reachable neighbors or null if lower layer is broken TODO no null?
