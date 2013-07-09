256c4a5668742af8642eea89cea66a574310e096
ThomasVolkert <thomas@homer-conferencing.com>
2013-07-09 13:00:35 +0200

    - fixed: documentation

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/clustering/IVirtualNode.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/clustering/IVirtualNode.java
@@ -13,12 +13,9 @@ import de.tuilmenau.ics.fog.facade.Name;
 import de.tuilmenau.ics.fog.routing.naming.hierarchical.HRMID;
 
 /**
- * This class is used to represent nodes within the HRM system. Such a virtual node can be either clusters 
- * to which addresses are assigned to or it can be connection end points that are associated to participants of the
- * clusters at the lowest hierarchical level. 
- * 
- * It is called virtual node because it can be either a physical node or a super node that aggregates multiple nodes 
- * (physical nodes) or super nodes (clusters). 
+ * This class is used to identify nodes within the HRM routing. Such an identifier can be either for a cluster, 
+ * to which addresses are assigned, or for a CEP that is associated to members of a clusters at the lowest hierarchical level. 
+ * It can be either a physical node or a logical node (a cluster) that aggregates multiple physical nodes. 
  */
 public interface IVirtualNode extends Name
 {
