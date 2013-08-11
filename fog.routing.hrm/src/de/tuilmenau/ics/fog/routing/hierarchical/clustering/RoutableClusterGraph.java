5d660ba9a61db5bee72b7855662c119a98130f58
ThomasVolkert <thomas@homer-conferencing.com>
2013-08-11 15:05:35 +0200

    - added: documentation - code cleanup: removed unnecessary code

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/clustering/RoutableClusterGraph.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/clustering/RoutableClusterGraph.java
@@ -10,15 +10,12 @@
 package de.tuilmenau.ics.fog.routing.hierarchical.clustering;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
 import de.tuilmenau.ics.fog.ui.Logging;
-import de.tuilmenau.ics.fog.util.Tuple;
 import de.tuilmenau.ics.graph.RoutableGraph;
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
-import edu.uci.ics.jung.graph.UndirectedSparseGraph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 
 /**
@@ -31,8 +28,7 @@ public class RoutableClusterGraph<NodeObject, LinkObject> extends RoutableGraph<
 {
 	public RoutableClusterGraph()
 	{
-		super();
-		mNodes =  new UndirectedSparseGraph<NodeObject, LinkObject>(); 
+		super(null);
 	}
 
 	/**
@@ -42,30 +38,36 @@ public class RoutableClusterGraph<NodeObject, LinkObject> extends RoutableGraph<
 	 */
 	public synchronized Collection<NodeObject> getNeighbors(NodeObject pSource)
 	{
-		return (mNodes.containsVertex(pSource) ? mNodes.getNeighbors(pSource) : new LinkedList<NodeObject>());
+		return (mRoutingGraph.containsVertex(pSource) ? mRoutingGraph.getNeighbors(pSource) : new LinkedList<NodeObject>());
 	}
 	
 	/**
-	 * Links two nodes. If nodes not exists, they will be created.
-	 * Method DOES check whether links had any kind of connection before
+	 * This method registers a link between two nodes in the routing graph. 
+	 * If the nodes don't exist in the routing graph, they are registered implicitly.
 	 * 
-	 * @param pFrom link starts at
-	 * @param pTo links end at
-	 * @param pLinkValue link object
+	 * @param pFrom starting point of the link
+	 * @param pTo the ending point of the link
+	 * @param pLinkObject the link object
 	 */
 	@Override
-	public synchronized void storeLink(NodeObject pFrom, NodeObject pTo, LinkObject pLinkValue)
+	public synchronized void storeLink(NodeObject pFrom, NodeObject pTo, LinkObject pLinkObject)
 	{
-		if((pFrom != null) && (pTo != null) && (pLinkValue != null)) {
-			// get equivalent object used for map for pFrom and pTo:
+		// check if parameters are valid
+		if((pFrom != null) && (pTo != null) && (pLinkObject != null)) {
+			// make sure the starting point is known
 			pFrom = add(pFrom);
+			
+			// make sure the ending point is known
 			pTo = add(pTo);
-			if(isLinked(pFrom, pTo)) {
-				return;
-			}
-			if(! mNodes.getNeighbors(pFrom).contains(pTo)) {
-				if(mNodes.addEdge(pLinkValue, pFrom, pTo, EdgeType.UNDIRECTED)) {
-					notifyObservers(new Event(EventType.ADDED, pLinkValue));
+			
+			// check if link already exists
+			if(!isLinked(pFrom, pTo)) {
+				// check if their already exist a link between these two nodes
+				if(!mRoutingGraph.getNeighbors(pFrom).contains(pTo)) {
+					// add the link to the routing graph
+					if(mRoutingGraph.addEdge(pLinkObject, pFrom, pTo, EdgeType.UNDIRECTED)) {
+						notifyObservers(new Event(EventType.ADDED, pLinkObject));
+					}
 				}
 			}
 		}
@@ -82,20 +84,19 @@ public class RoutableClusterGraph<NodeObject, LinkObject> extends RoutableGraph<
 	{
 		List<LinkObject> tPath = null;
 		
-		//Logging.log(this, "Searching for a route from " + (pFrom instanceof AttachedCluster ? ((AttachedCluster)pFrom).getClusterDescription() : pFrom ) + " to " + (pTo instanceof AttachedCluster ? ((AttachedCluster)pTo).getClusterDescription() : pTo));
+		LinkedList<NodeObject> tNodes = new LinkedList<NodeObject>();
+		
 		pFrom = containsVertex(pFrom);
 		pTo = containsVertex(pTo);
-
-		LinkedList<NodeObject> tNodes=new LinkedList<NodeObject>();
 		
 		if((pFrom != null) && (pTo != null)) {
-			DijkstraShortestPath<NodeObject, LinkObject> tAlg = new DijkstraShortestPath<NodeObject, LinkObject>(mNodes);
-			tPath = tAlg.getPath(pFrom, pTo);
+			DijkstraShortestPath<NodeObject, LinkObject> tRoutingAlgo = new DijkstraShortestPath<NodeObject, LinkObject>(mRoutingGraph);
+			tPath = tRoutingAlgo.getPath(pFrom, pTo);
 			
 			NodeObject tTarget = pFrom;
 			
 			for(LinkObject tLink : tPath) {
-				tTarget = getDest(tTarget, tLink);
+				tTarget = getLinkEndNode(tTarget, tLink);
 				tNodes.add(tTarget);
 			}
 		}
@@ -111,12 +112,12 @@ public class RoutableClusterGraph<NodeObject, LinkObject> extends RoutableGraph<
 	 * @param pLink This is the link you wish to know one end point while providing the other one.
 	 * @return Other end point of the link is provided.
 	 */
-	public NodeObject getDest(NodeObject pSource, LinkObject pLink)
+	public NodeObject getLinkEndNode(NodeObject pSource, LinkObject pLink)
 	{
 		try {
-			return mNodes.getOpposite(pSource, pLink);
+			return mRoutingGraph.getOpposite(pSource, pLink);
 		} catch (IllegalArgumentException tExc) {
-			Logging.err(this, pSource + " is not incident to " + pLink + "(" + mNodes.getIncidentVertices(pLink), tExc);
+			Logging.err(this, pSource + " is not incident to " + pLink + "(" + mRoutingGraph.getIncidentVertices(pLink), tExc);
 		}
 		return null;
 		
@@ -124,20 +125,26 @@ public class RoutableClusterGraph<NodeObject, LinkObject> extends RoutableGraph<
 	
 
 	/**
-	 * Check whether two object are connected.
+	 * Check whether a link between two nodes are already known.
 	 * 
-	 * @return true in case first and second object are linked.
+	 * @param pFirst the first node
+	 * @param pSecond the second node
+	 * @return true if a link is known, otherwise false
 	 */
 	public boolean isLinked(NodeObject pFirst, NodeObject pSecond)
 	{
-		if(mNodes.containsVertex(pFirst)) {
-			return mNodes.getNeighbors(pFirst).contains(pSecond);
+		if(mRoutingGraph.containsVertex(pFirst)) {
+			return mRoutingGraph.getNeighbors(pFirst).contains(pSecond);
 		} else {
 			return false;
 		}
 	}
 	
-	
+	/**
+	 * Return a descriptive string
+	 * 
+	 * @return the descriptive string
+	 */
 	public String toString()
 	{
 		return getClass().getSimpleName();
@@ -147,60 +154,16 @@ public class RoutableClusterGraph<NodeObject, LinkObject> extends RoutableGraph<
 	public synchronized List<LinkObject> getRoute(NodeObject pFrom, NodeObject pTo)
 	{
 		List<LinkObject> tPath = null;
-		
-		//Logging.log(this, "Searching for a route from " + (pFrom instanceof AttachedCluster ? ((AttachedCluster)pFrom).getClusterDescription() : pFrom ) + " to " + (pTo instanceof AttachedCluster ? ((AttachedCluster)pTo).getClusterDescription() : pTo));
+
 		pFrom = containsVertex(pFrom);
 		pTo = containsVertex(pTo);
 
 		if((pFrom != null) && (pTo != null)) {
-			DijkstraShortestPath<NodeObject, LinkObject> tAlg = new DijkstraShortestPath<NodeObject, LinkObject>(mNodes);
-			tPath = tAlg.getPath(pFrom, pTo);
+			// use Djikstra over the routing graph
+			DijkstraShortestPath<NodeObject, LinkObject> tRoutingAlgo = new DijkstraShortestPath<NodeObject, LinkObject>(mRoutingGraph);
+			tPath = tRoutingAlgo.getPath(pFrom, pTo);
 		}
 
 		return tPath;
 	}
-
-	/**
-	 * Use this method if you want to know a route from one node to another and some nodes are not allowed to be routed along
-	 * 
-	 * @param pSource This is the source of the path you wish to know.
-	 * @param pTarget This is the target of the path you want to know
-	 * @param pIgnoredNodes Please provide a set of nodes that are not allowed to be used for route determination.
-	 * @return
-	 */
-//	public List<LinkObject> getRouteWithInvalidatedNodes(NodeObject pSource, NodeObject pTarget, LinkedList<NodeObject> pIgnoredNodes)
-//	{
-//		HashMap<NodeObject, Collection<LinkObject>> tLinksOnIgnoredNode = new HashMap<NodeObject, Collection<LinkObject>>();
-//		HashMap<NodeObject, LinkedList<Tuple<NodeObject, NodeObject>>> tNeighborsOfIgnoredNode = new HashMap<NodeObject, LinkedList<Tuple<NodeObject, NodeObject>>>();
-//		
-//		for(NodeObject tNode : pIgnoredNodes) {
-//			LinkedList<LinkObject> tLinks = new LinkedList<LinkObject>();
-//			for(LinkObject tLink : mNodes.getIncidentEdges(tNode)) {
-//				tLinks.add(tLink);
-//			}
-//			tLinksOnIgnoredNode.put(tNode, tLinks);
-//			LinkedList<Tuple<NodeObject, NodeObject>> tPairs = new LinkedList<Tuple<NodeObject, NodeObject>>();
-//			tNeighborsOfIgnoredNode.put(tNode, tPairs);
-//			for(LinkObject tLink : tLinks) {
-//				tPairs.add(new Tuple<NodeObject, NodeObject>(tNode, mNodes.getOpposite(tNode, tLink)));
-//				Logging.log(this, "Removed connection between " + tNode + " and " + mNodes.getOpposite(tNode, tLink));
-//				mNodes.removeEdge(tLink);
-//			}
-//			mNodes.removeVertex(tNode);
-//			Logging.log(this, "Removed node " + tNode);
-//		}
-//		
-//		List<LinkObject> tPath = getRoute(pSource, pTarget);
-//		Logging.log(this, "Calculated restricted route " + tPath + " which is from " + pSource + " to " + pTarget);
-//		
-//		for(NodeObject tNode : pIgnoredNodes) {
-//			add(tNode);
-//			for(LinkObject tLink : tLinksOnIgnoredNode.get(tNode)) {
-//				mNodes.addEdge(tLink, tNeighborsOfIgnoredNode.get(tNode).getFirst().getFirst(), tNeighborsOfIgnoredNode.get(tNode).getFirst().getSecond(), EdgeType.UNDIRECTED);
-//				tNeighborsOfIgnoredNode.get(tNode).removeFirst();
-//			}
-//		}
-//		
-//		return tPath;
-//	}
 }
