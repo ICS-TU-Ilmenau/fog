70a22550d908cd9125437ef6d08db600cb38a735
ThomasVolkert <thomas@homer-conferencing.com>
2013-09-12 17:04:33 +0200

    -

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/packets/hierarchical/topology/TopologyReport.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/packets/hierarchical/topology/TopologyReport.java
@@ -9,19 +9,22 @@
  ******************************************************************************/
 package de.tuilmenau.ics.fog.packets.hierarchical.topology;
 
+import de.tuilmenau.ics.fog.facade.Name;
+import de.tuilmenau.ics.fog.packets.hierarchical.SignalingMessageHrm;
+
 /**
  * PACKET: This packet is used within the HRM "report" phase. 
  * 		   Either a coordinator uses this packet in order to report topology to a superior coordinator,
  * 		   or a cluster member of base hierarchy level uses this packet to report topology to its coordinator.
  */
-public class TopologyReport
+public class TopologyReport extends SignalingMessageHrm
 {
 
 	/**
 	 * Constructor
 	 */
-	public TopologyReport()
+	public TopologyReport(Name pSenderName, Name pReceiverName)
 	{
-		
+		super(pSenderName, pReceiverName);
 	}
 }
