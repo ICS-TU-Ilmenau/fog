afc40a67972b721604c0da021f7fc2d296d93d60
ThomasVolkert <thomas@homer-conferencing.com>
2013-09-17 20:29:13 +0200

    - added: HRMConfig: activated again the auto. cleanup(closing) of connections

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/HRMConfig.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/HRMConfig.java
@@ -147,7 +147,7 @@ public class HRMConfig
 		/**
 		 * Defines if connection should remain open or be automatically closed if the last inferior comm. channel was closed
 		 */
-		public static final boolean AUTO_CLEANUP_FOR_CONNECTIONS = false;
+		public static final boolean AUTO_CLEANUP_FOR_CONNECTIONS = true;
 	}
 	
 	/**
