6053f4aa369ec575ff30825f62cb984dbc29f99a
ThomasVolkert <thomas@homer-conferencing.com>
2013-10-03 12:29:38 +0200

    - added: BullyPriorityUpdate: implement ISignalingMessageHrmBroadcastable and allow broadcasts

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/packets/hierarchical/election/BullyPriorityUpdate.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/packets/hierarchical/election/BullyPriorityUpdate.java
@@ -10,14 +10,17 @@
 package de.tuilmenau.ics.fog.packets.hierarchical.election;
 
 import de.tuilmenau.ics.fog.facade.Name;
+import de.tuilmenau.ics.fog.packets.hierarchical.ISignalingMessageHrmBroadcastable;
+import de.tuilmenau.ics.fog.packets.hierarchical.SignalingMessageHrm;
 import de.tuilmenau.ics.fog.routing.hierarchical.election.BullyPriority;
 import de.tuilmenau.ics.fog.routing.naming.hierarchical.HRMID;
+import de.tuilmenau.ics.fog.ui.Logging;
 
 /**
  * PACKET: If the priority of a node changes this object has to be used to inform other cluster members about the change.
  * 		   The packet has to be send as broadcast.
  */
-public class BullyPriorityUpdate extends SignalingMessageBully //TV
+public class BullyPriorityUpdate extends SignalingMessageBully implements ISignalingMessageHrmBroadcastable
 {
 	private static final long serialVersionUID = -8819106581802846812L;
 	
@@ -30,5 +33,22 @@ public class BullyPriorityUpdate extends SignalingMessageBully //TV
 	public BullyPriorityUpdate(Name pSenderName, BullyPriority pSenderPriroity)
 	{
 		super(pSenderName, HRMID.createBroadcast(), pSenderPriroity);
+	}
+
+	/**
+	 * Returns a duplicate of this packet
+	 * 
+	 * @return the duplicate packet
+	 */
+	@Override
+	public SignalingMessageHrm duplicate()
+	{
+		BullyPriorityUpdate tResult = new BullyPriorityUpdate(getSenderName(), getSenderPriority());
+		
+		super.duplicate(tResult);
+
+		Logging.log(this, "Created duplicate packet: " + tResult);
+		
+		return tResult;
 	}
 }
