cb42ef43166946a7a1b51ae7316955db20e9258d
ThomasVolkert <thomas@homer-conferencing.com>
2013-12-01 19:26:09 +0100

    -

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/management/ControlEntity.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/management/ControlEntity.java
@@ -159,7 +159,7 @@ public abstract class ControlEntity implements AbstractRoutingGraphNode, Localiz
 			// update to the new one
 			mBullyPriority = pPriority;
 			
-			Logging.log(this, "ASSIGNED BULLY PRIORITY for cluster " + toString() + " updated from " + (tBullyPriority != null ? tBullyPriority.getValue() : "null") + " to " + (mBullyPriority != null ? mBullyPriority.getValue() : "null"));
+			Logging.log(this, "ASSIGNED BULLY PRIORITY for " + toString() + " updated from " + (tBullyPriority != null ? tBullyPriority.getValue() : "null") + " to " + (mBullyPriority != null ? mBullyPriority.getValue() : "null"));
 		}
 	}
 
