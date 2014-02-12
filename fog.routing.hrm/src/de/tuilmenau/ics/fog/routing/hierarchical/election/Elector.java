59c5b47d6dfddaeb7cb8c9f9c10ce0ad3c0788ee
ThomasVolkert <thomas@homer-conferencing.com>
2014-02-12 17:24:39 +0100

    - fixed: Elector: eventReset() calls recheckLocalClusterIsAllowedToWin() in every case - independent from if the entity is an active ClusterMember

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/election/Elector.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/election/Elector.java
@@ -323,8 +323,9 @@ public class Elector implements Localization
 				Logging.err(this, "Haven't found parent in the list of active ClusterMembers (but it should be there), error in state machine, parent is: " + mParent);
 			}
 			
-			recheckLocalClusterIsAllowedToWin(this + "::eventInvalidation()\n   ^^^^" + pCause);
 		}
+
+		recheckLocalClusterIsAllowedToWin(this + "::eventInvalidation()\n   ^^^^" + pCause);
 	}
 	
 	/**
