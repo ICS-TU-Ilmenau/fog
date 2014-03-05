fb393450b1f8988e73674fa298a658aa0905e3a5
ThomasVolkert <thomas@homer-conferencing.com>
2014-03-05 15:16:26 +0100

    - fixed: Coordiantor: avoid misleading debug output

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/management/Coordinator.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/management/Coordinator.java
@@ -1439,7 +1439,7 @@ public class Coordinator extends ControlEntity implements Localization, IEvent
 
 		if((pHRMID != null) && (!pHRMID.isZero())){
 			if(getHierarchyLevel().getValue() == HRMConfig.Hierarchy.HEIGHT - 2){
-				if(getHRMID() != null){
+				if((getHRMID() != null) && (!getHRMID().isZero())){
 					if(!pHRMID.equals(getHRMID())){
 						Logging.warn(this, "Detected an address change at the top of the address hierarchy from " + getHRMID() + " to " + pHRMID);
 					}
