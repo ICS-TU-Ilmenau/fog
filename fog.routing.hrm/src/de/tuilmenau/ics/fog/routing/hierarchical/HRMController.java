18f6bcf299e461de9f89d6c3a61c1b70f5948211
ThomasVolkert <thomas@homer-conferencing.com>
2014-04-14 17:21:54 +0200

    - fixed: HRMController: avoid dead lock in isGUIFormerCoordiantorID()

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/HRMController.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/HRMController.java
@@ -1782,7 +1782,7 @@ public class HRMController extends Application implements ServerCallback, IEvent
 	 * 
 	 * @return true or false
 	 */
-	public synchronized boolean isGUIFormerCoordiantorID(long pCoordinatorID)
+	public boolean isGUIFormerCoordiantorID(long pCoordinatorID)
 	{
 		boolean tResult = false;
 		
@@ -5104,7 +5104,7 @@ public class HRMController extends Application implements ServerCallback, IEvent
 					 */
 					if(tCoordinator.getHierarchyLevel().isHighest()){
 						Logging.warn(this, "validateResults() found a top coordinator on: " + getNodeGUIName());
-//						if(!getNodeGUIName().equals("node1")){
+//						if(!getNodeGUIName().equals("node9")){
 //							tResult = false;
 //						}
 						synchronized (sRegisteredTopCoordinatorsCounter) {
@@ -5255,8 +5255,8 @@ public class HRMController extends Application implements ServerCallback, IEvent
 						/**
 						 * HRM test app
 						 */
-	//					HRMTestApp tHRMTestApp = new HRMTestApp(getNode());
-	//					tHRMTestApp.start();
+//						HRMTestApp tHRMTestApp = new HRMTestApp(getNode());
+//						tHRMTestApp.start();
 						
 						/**
 						 * auto-exit simulation
