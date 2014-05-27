af1ece3bb7550c796220ef828f2d8b626c5626d1
ThomasVolkert <thomas@homer-conferencing.com>
2014-05-27 15:48:32 +0200

    - fixed: ProbeRouting: createProbeRoutingConnection() waits 60 secs for a connection

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/ui/eclipse/commands/hierarchical/ProbeRouting.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/ui/eclipse/commands/hierarchical/ProbeRouting.java
@@ -287,7 +287,7 @@ public class ProbeRouting extends EclipseCommand
 		BlockingEventHandling tBlockingEventHandling = new BlockingEventHandling(tConnection, 1);
 		
 		// wait for the first event
-		Event tEvent = tBlockingEventHandling.waitForEvent(HRMConfig.Hierarchy.CONNECT_TIMEOUT);
+		Event tEvent = tBlockingEventHandling.waitForEvent(60 /* seconds */);
 		Logging.log(pCaller, "        ..=====> got connection " + pTargetNodeHRMID + " event: " + tEvent);
 		
 		if(tEvent != null){
