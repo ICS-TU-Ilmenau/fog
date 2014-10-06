ad87e4d09a27ef8cea974ad7b8b1b567b4cd17d5
ThomasVolkert <thomas@homer-conferencing.com>
2014-10-06 14:59:46 +0200

    - fixed: HRM: code cleanup for activator

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/Activator.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/Activator.java
@@ -14,26 +14,27 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-	private static BundleContext context;
+	private static BundleContext mContext;
 
 	static BundleContext getContext() {
-		return context;
+		return mContext;
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	public void start(BundleContext bundleContext) throws Exception {
-		Activator.context = bundleContext;
+	public void start(BundleContext pBundleInstance) throws Exception {
+		Activator.mContext = pBundleInstance;
+		System.out.println("################################ Started HRM bundle: " + pBundleInstance);
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext bundleContext) throws Exception {
-		Activator.context = null;
+	public void stop(BundleContext pBundleInstance) throws Exception {
+		Activator.mContext = null;
 	}
 
 }
