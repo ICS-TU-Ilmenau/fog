965532f9b35a19c5c1c7bf86bdb124a8d4087761
ThomasVolkert <thomas@homer-conferencing.com>
2015-05-04 11:41:35 +0200

    - fixed: HRMControllerProcessor: removed auto-generated TODO entry

--- fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/HRMControllerProcessor.java
+++ fog.routing.hrm/src/de/tuilmenau/ics/fog/routing/hierarchical/HRMControllerProcessor.java
@@ -477,7 +477,6 @@ public class HRMControllerProcessor extends Thread
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
